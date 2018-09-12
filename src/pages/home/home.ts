850573a4af199dadb3269d8f44ba51775db15453
Cameron Hynes <cameron.w.hynes@gmail.com>
2018-09-12 22:14:38 +1200

    Added Flashlight Toggle

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  timerSubscription: any;
+
+  constructor(
+    public navCtrl: NavController,
+    private flashlight: Flashlight) {
+
+  }
+
+  ionViewDidEnter() {
+
+    let timer = Observable.timer(1, 10);
+
+    this.timerSubscription = timer.subscribe((t: any) => {
+      this.flashlight.toggle();
+    });
 
   }
 
